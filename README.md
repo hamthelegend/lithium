8011bd3c6cf4a332dea0ae60f648b19f03e2698b
hamthelegend <hamuelxd@gmail.com>
2023-12-24 02:06:19 +0800

    Added screenshots

---
+++ README.md
@@ -0,0 +1,6 @@
+## Screenshots
+
+![img.png](screenshots/img.png)
+![img_2.png](screenshots/img_2.png)
+![img_3.png](screenshots/img_3.png)
+![img_1.png](screenshots/img_1.png)
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.thebrownfoxx.lithium"
         minSdk = 27
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
